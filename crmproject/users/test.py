95b60e9fa80714390c078c130f284b73547fee1b
Shamim <shamim41.cse@gmail.com>
2025-06-03 15:39:57 +0200

    create user

--- crmproject/users/test.py
+++ crmproject/users/test.py
@@ -2,3 +2,14 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 from .models import Order
+
+User = get_user_model()
+
+class OrderViewTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.customer = User.objects.create_user(
+            username='customeruser',
+            password='customerpassword',
+            user_type= 'customer'
+        )
