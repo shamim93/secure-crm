300b8407f939acc105b34511ddf2f95f5ed05935
Shamim <shamim41.cse@gmail.com>
2025-06-05 13:24:41 +0200

    Cookies configured

--- crmproject/crmproject/settings.py
+++ crmproject/crmproject/settings.py
@@ -117,6 +117,15 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Cookies setup
+SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_HTTPONLY = True
+SESSION_COOKIE_SAMESITE = 'Lax'
+
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_HTTPONLY = True
+CSRF_COOKIE_SAMESITE = 'Lax'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
